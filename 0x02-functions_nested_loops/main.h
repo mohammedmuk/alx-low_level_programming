c39a7427b7382e9abcdb9ff93838ec385cf3f018
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-14 07:14:17 -0800

    modify main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,17 +2,21 @@
 #define MAIN_H
 
 #include "main.h"
+#include <unistd.h>
 #include <stdio.h>
 
-int _putchar(char c);
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+};
 void print_alphabet()
 {
 int ch;
 for (ch ='a'; ch <= 'z'; ch++)
 {
-putchar(ch);
+_putchar(ch);
 }
-putchar('\n');
+_putchar('\n');
 }
 
 #endif
