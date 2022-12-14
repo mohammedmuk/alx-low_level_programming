2b060fa23203d958139f4b52c0a0f86565523ae1
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-14 09:56:18 -0800

    modify 3-islower.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,21 +3,18 @@
 
 /**
  * _islower - check parameter is equal lowercase or not
+ *
  * @c: The character to print
  * Return: Always 0 (Success).
  */
 int _islower(int c)
 {
-int ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-if (ch == c)
+if (c > 'a' || c < 'z')
 {
 return (1);
 }
-else if (c == 122)
+else
 {
 return (0);
-}
 }
 }
