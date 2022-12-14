06ebde990b9999184501689f3325102144275a1d
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-14 12:11:36 -0800

    Add 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abc - return positive data
+ *
+ * @n: number is returned
+ * Return: n
+ */
+int _abc(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
