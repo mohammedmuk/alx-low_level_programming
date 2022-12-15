149768e09ad6a5396064dc08f617e3b3f7f71a21
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-15 10:14:32 -0800

    Add -mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - return multiplies numbers
+ * @num1: number 1
+ * @num2: number 2
+ * Return: num1 * num2
+ */
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
