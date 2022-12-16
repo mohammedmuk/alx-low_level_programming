7b7c03cb12c8f8291acefee1fdb7e7f7c506f561
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-16 12:50:45 -0800

    modify 8-print_square.c file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,20 @@ void print_square(int size)
 {
 	int h, w;
 
-	for (h = 0; h <= size; h++)
+	for (h = 1; h <= size; h++)
 	{
-		for (w = 0; w <= size; w++)
+		for (w = 1; w <= size; w++)
 		{
 			putchar('#');
 		}
 		putchar('\n');
 	}
+}
+
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
 }
