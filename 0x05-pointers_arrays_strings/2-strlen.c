6f2e2370139d2a548a2f95744c7e7c621fd6ce70
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-19 10:02:22 -0800

    small modify

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,17 +4,17 @@
 /**
  * _strlen - return length of string
  * @s: string
+ * @num: number
  *
  * Return: num
  */
 
-int _strlen(char s[])
+int _strlen(char s[], int num)
+	num = 0;
 
-	int num = 0;
-
-	while(s[num] != '\0')
+	while (s[num] != '\0')
 	{
 		num++;
 	}
-	return num;
+	return (num);
 }
