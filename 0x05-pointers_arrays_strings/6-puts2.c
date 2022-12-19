7c3d6298b239ebd8454e42888d8f57e542deaf9b
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-19 14:10:15 -0800

    Add 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - print coupled numbers
+ * @str: string
+ *
+ * Return: no return
+ */
+
+void puts2(char *str)
+{
+	int num;
+
+	for (num = 0; num < strlen(str); num++)
+	{
+		if (str[num] % 2 == 0)
+		{
+			putchar(str[num]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
