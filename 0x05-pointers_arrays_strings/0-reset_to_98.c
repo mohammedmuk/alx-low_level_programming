434b18781555861508af0256a3f38fa5acfabb49
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-19 04:02:59 -0800

    Add 0x05-pointers_arrays_strings directory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - apdate the value
+ * @n: number
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "main.h"
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+#endif
