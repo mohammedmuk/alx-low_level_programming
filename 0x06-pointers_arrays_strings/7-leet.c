766ef3f50904d3004e14a672b49d59d4e360c115
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-22 13:32:34 -0800

    Add  7-leet.c file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - encode function
+ * @s: string
+ *
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int stringNum, lettNum;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+	stringNum = 0;
+
+	while (s[stringNum] != '\0')
+	{
+		leetNum = 0;
+
+		while (lettNum < 10)
+		{
+			if (leetLetters[leetNum] == s[stringNum])
+			{
+				s[stringNum] = leetNums[leetNum];
+			}
+			leetNum++;
+		}
+		stringNum++;
+	}
+	return (s);
+}
