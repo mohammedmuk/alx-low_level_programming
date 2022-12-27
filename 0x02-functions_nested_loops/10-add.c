0d14c4c441dac0ad1693f8b19776523d933566ff
mohammedmuk <mohammedmuk20@gmail.com>
2022-12-27 13:58:05 -0800

    Add 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * add - add to interger and return a new value
+ *@num1: first number
+ *@num2: second number
+ *
+ * Return: num1 + num2
+ */
+
 int add(int num1, int num2)
 {
 	return (num1 + num2);
