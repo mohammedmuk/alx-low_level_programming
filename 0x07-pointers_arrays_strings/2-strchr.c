985d84a6c921b207cbbea45e2a9bf5f4fd7a0cdd
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-03 11:22:45 -0800

    add 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - cut the string
+ * @s: string 1
+ * @c: string 2
+ *
+ * Return: arrc
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, n;
+	char arrc[100];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			for (n = 0; s[i] != '\0'; n++)
+			{
+				arrc[n] = s[i];
+				i++;
+			}
+			break;
+		}
+	}
+}
