176ea9d5470295d8b965a87a8a773f43e606883a
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-04 05:22:23 -0800

    add  4-pow_recursion.c file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - x raised to the power of y
+ * @x: number 1
+ * @y: number 2
+ *
+ * Return: (x * _pow_recursion(x, y - 1))
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
