f3aea8fbee220cd033cc29c50fac962a4427596d
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-04 08:19:03 -0800

    add 5-sqrt_recursion.c file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * sqr -  sqrt number
+ * @n: number1
+ * @i: number2
+ * Return: sqr(n ,i + 1)
+ */
+
+int sqr(int n, int i)
+{
+	int sq = i * i;
+
+	if (sq > n)
+	{
+		return (-1);
+	}
+
+	else if (sq == n)
+	{
+		return (i);
+	}
+
+	return (sqr(n, i + 1));
+}
+/**
+ * _sqrt_recursion - return sqr function
+ * @n: number
+ * Return: sqr(n, i)
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	return (sqr(n, i));
+}
