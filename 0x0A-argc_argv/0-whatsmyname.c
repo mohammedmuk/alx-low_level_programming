c1f4318cf5033c698b9b44051b8c150a6d039585
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-10 10:48:34 -0800

    new modify

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Entry point function
+ * @argc: number of arrgument
+ * @argv: arrguments in array
+ *
+ * Return: always 0 success
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
