a32699ca1d125161c06c60f2495346f9839b5760
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-28 11:02:07 -0800

    modify dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,10 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+typedef struct dog
+{
+	char *name, *owner;
+	float age;
+} dog_t;
+
 #endif
