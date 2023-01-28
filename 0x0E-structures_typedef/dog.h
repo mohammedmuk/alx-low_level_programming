875959dffa724547c4c7be6c7c3cc8a8a240a6cd
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-28 06:10:56 -0800

    new modify

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,19 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - function for struct
+ * @name: for name
+ * @age: for age
+ * @owner: for owner
+ *
+ * Return: no return
+ */
+
 struct dog
 {
-	char *name, *owner ; float age ;
+	char *name, *owner;
+	float age;
 };
+
+endif
