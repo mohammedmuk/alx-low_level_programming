4f7d74751992c30d99ab62f33b60a5a6af509529
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-28 10:43:50 -0800

    new modify

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,8 @@ void print_dog(struct dog *d)
 	float age;
 	char *owner;
 
+	if (d != NULL)
+{
 	name = d->name;
 	age = d->age;
 	owner = d->owner;
@@ -26,4 +28,5 @@ void print_dog(struct dog *d)
 		printf("Name: %s\nAge: (nil)\nOwner: %s\n", name, owner);
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", name, age, owner);
+}
 }
