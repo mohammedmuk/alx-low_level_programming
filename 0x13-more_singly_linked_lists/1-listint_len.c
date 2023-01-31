12994f7ba97ae103943b95363b7df944d90082c4
mohammedmuk <mohammedmuk20@gmail.com>
2023-01-31 11:54:37 -0800

    new edit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
+/**
+ * listint_len - return number of item of struct
+ * @h : struct item
+ *
+ * Return: count
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	int count = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		count++;
 		h = h->next;
