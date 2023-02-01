eedee0d5222bd49bf5638e9356b8645138ab7077
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-01 06:55:05 -0800

    new edit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_listint - free linked list
+ * @head: head of linked list
+ *
+ * Return: void
+ */
+
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
