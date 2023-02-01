d3c0d6bf7c405843d9460743dcbc8c0051836cac
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-01 04:02:41 -0800

    add 2-add_nodeint.c file

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - add new node to linked list
+ * @head:  head of linked list
+ * @n: number
+ *
+ * Return: tmp
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *tmp;
+
+	tmp = malloc(sizeof(listint_t));
+
+	tmp->n = n;
+	tmp->next = *head;
+
+	*head = tmp;
+
+	return (tmp);
+}
