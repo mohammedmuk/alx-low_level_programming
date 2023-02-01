4548184a4836058d1fa545562d6a01d74c225f96
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-01 12:55:45 -0800

    new edit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	while (head)
+	while (*head)
 	{
-		tmp = tmp->next;
-		free(tmp);
+		tmp = (*head)->next;
+		free(*head);
 		*head = tmp;
 	}
 
-	*head = NULL;
+	head = NULL;
 }
