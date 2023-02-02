9334576587b406f0d8517a6e4da634eb29da8e2d
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-02 13:28:36 -0800

    new modify

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,8 +17,10 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = strlen(b) - 1; i >= 0; i--)
 	{
-		if (b[i] != '1' && b[i] == '0')
+
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
+
 		if (b[i] == '1')
 		{
 			num += count;
