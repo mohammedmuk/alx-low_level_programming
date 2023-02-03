85e262d9141f3f8c9309b5a3c35867eddccfd2fe
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-03 13:07:50 -0800

    add new file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  _strlen - funcion lent of String.
+ *  @str: Input String.
+ *  Return: length.
+ */
+
+char *_strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+
+	return (i);
+}
+
+/**
+ *  _strdup - Copy an string.
+ *  @str: Input String.
+ *  Return: copy string.
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		i = _strlen(str);
+		a = malloc(i * sizeof(char) + 1);
+		if (a == NULL)
+			return (0);
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			a[j] = str[j];
+		}
+
+		return (a);
+	}
+}
