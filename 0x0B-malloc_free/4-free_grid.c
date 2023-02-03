ded71104da0d86296e1f77799a823dd12b49293b
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-03 13:24:04 -0800

    new file

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - grid for int.
+ * @grid: the address grid
+ * @height: height of the grid
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = height - 1; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+	grid = NULL;
+}
