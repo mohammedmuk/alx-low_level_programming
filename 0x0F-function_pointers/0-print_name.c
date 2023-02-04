292442a61e707547d26bf9b15c8d0199fec8d306
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-04 10:48:03 -0800

    new directory

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: if is null.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if ((name == NULL) || (f == NULL))
+		return;
+	f(name);
+}
