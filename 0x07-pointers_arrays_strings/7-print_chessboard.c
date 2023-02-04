de57e940c068b391ebed1779ba95a78c3aec2cdd
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-04 08:51:23 -0800

    new file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Prints the chessboard
+ * @a: pointer to a row of the chessboard
+ *
+ * Return: Nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+	int size = sizeof(*a) / sizeof(char);
+	char r;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			r = (*(a + i))[j];
+			putchar(r);
+		}
+		putchar('\n');
+	}
+}
