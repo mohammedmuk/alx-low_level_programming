564747209f8353def39fec7e22fb6ae98448388e
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-06 14:21:21 -0800

    new file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -34,20 +34,20 @@ int _check_pal(char *s, int l)
 	}
 	else
 		return (0);
+}
+/**
+ * is_palindrome - Recurssive funct that checks a palindrome
+ * @s: type char str
+ * Return: 1 if palindrome 0 if not.
+ */
 
-	/**
-	 * is_palindrome - Recurssive funct that checks a palindrome
-	 * @s: type char str
-	 * Return: 1 if palindrome 0 if not.
-	 */
-
-	int is_palindrome(char *s)
-	{
+int is_palindrome(char *s)
+{
 		int l;
 
 		l = _strlen_recursion(s);
 		if (l <= 1)
 			return (1);
 		return (_check_pal(s, l));
-	}
 }
+
