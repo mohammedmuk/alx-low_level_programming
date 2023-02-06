187106054e73af6bed5afb2d71c75c9ddb180345
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-06 11:15:49 -0800

    new edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,18 +11,21 @@ int main(void)
 {
 	int i;
 	float a, b, r;
+
 	a = 1;
 	b = 2;
-	r= 0;
+	r = 0;
 
 	printf("1, 2");
-	for(i = 1; i <= 96; i++)
+
+	for (i = 1; i <= 96; i++)
 	{
 		r = a + b;
 		a = b;
 		b = r;
-		printf(", %.0f",r);
+		printf(", %.0f", r);
 	}
+
 	putchar('\n');
 	return (0);
 }
