b6c93761bdf1a5466e82c8c3293009af7f659ce7
mohammedmuk <mohammedmuk20@gmail.com>
2023-02-08 21:51:41 -0800

    new file

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_number - print number
+ * @n: number
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i;
+	char str[50];
+
+	sprintf(str, "%d", n);
+
+	for (i = 0; str[i] != '\0'; i++)
+		putchar(str[i]);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *x);
 char *cap_string(char *s);
 char *leet(char *s);
-
+void print_number(int n);
 #endif
